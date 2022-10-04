cdbecf35fa3fd2f2e4c714598c7bff6c189be1e1
lorelyrey <lorely.reyes@uttn.mx>
2022-10-03 20:11:10 -0600

    cambierutas

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-Route::get('/login', function () {
+Route::get('login', function () {
     return view('login');
 });
