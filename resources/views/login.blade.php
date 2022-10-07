f968dab5628bf3cb5e70d96cb77a7e0377bafdb3
BeltsasarAzpeitia <belsagallardo@hotmail.com>
2022-10-07 00:02:29 -0500

    #Mich Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,10 +1,7 @@
 @include('header')
-<<<<<<< HEAD
 <h1 class="md-12">
 
 </h1>
-=======
->>>>>>> eb729476b0a5aacee26042904412d42b75e7ba8f
 
 
     <!-- Login Section Begin -->
@@ -12,11 +9,8 @@
         <div class="container">
             <div class="row d-flex justify-content">
                 <div class="col-lg-6">
-                    <div class="login__form">
-                        <div class="azp-tittle">
-                            <h3>Login</h3>
-                        </div>
-
+                    <div class="login__form">  
+                        <h3 class="azp">Login</h3>
                         <form action="#">
                             <div class="input__item">
                                 <input type="text" placeholder="Email address">
@@ -53,23 +47,10 @@
                             </ul>
                         </div>
                     </div>
+    
                 </div>
             </div>
-            <div class="login__social">
-                <div class="row d-flex">
-                    <div class="col-lg-6">
-                        <div class="login__social__links">
-                            <span>or</span>
-                            <ul>
-                                <li><a href="#" class="facebook"><i class="fa fa-facebook"></i> Sign in With
-                                Facebook</a></li>
-                                <li><a href="#" class="google"><i class="fa fa-google"></i> Sign in With Google</a></li>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
         </div>
     </section>
     <!-- Login Section End -->
