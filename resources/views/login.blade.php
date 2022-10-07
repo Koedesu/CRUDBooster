417a109318dd5e5775caa7e6f61dfc5d1c1fa1df
pic4fr3sa <michelle.velez@uttn.mx>
2022-10-06 23:40:49 -0500

    #login Michi

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,12 +3,14 @@
 
     <!-- Login Section Begin -->
     <section class="login spad">
-        <div id="cuadro_login"></div>
         <div class="container">
-            <div class="row">
+            <div class="row d-flex justify-content">
                 <div class="col-lg-6">
                     <div class="login__form">
-                        <h3>Login</h3>
+                        <div class="azp-tittle">
+                            <h3>Login</h3>
+                        </div>
+
                         <form action="#">
                             <div class="input__item">
                                 <input type="text" placeholder="Email address">
@@ -18,32 +20,53 @@
                                 <input type="text" placeholder="Password">
                                 <span class="icon_lock"></span>
                             </div>
-                            <button type="submit" class="site-btn">Login</button>
+                            <div class="login__form">
+                                <button type="submit" class="site-btn">Login</button>
+                                </div>
                         </form>
-
+                        <!-- <a href="#" class="forget_pass">Forgot Your Password?</a> -->
                     </div>
-                    <div class="login__social">
-                <div class="row d-flex justify-content-center">
+                </div>
+            <!--     <div class="col-lg-6">
+                    <div class="login__register">
+                        <h3>Dont’t Have An Account?</h3>
+                        <a href="#" class="primary-btn">Register Now</a>
+                    </div>
+                </div> -->
+            </div>
+            <div class="login__social">
+                <div class="row d-flex">
                     <div class="col-lg-6">
                         <div class="login__social__links">
                             <span>or</span>
                             <ul>
-                                <li><a href="#" class="facebook"><i class="fa fa-facebook"></i> Sign in with
+                                <li><a href="#" class="facebook"><i class="fa fa-facebook"></i> Sign in With
                                 Facebook</a></li>
-                                <li><a href="#" class="google"><i class="fa fa-google"></i> Sign in with Google</a></li>
-
-
+                                <li><a href="#" class="google"><i class="fa fa-google"></i> Sign in With Google</a></li>
+                                </li>
                             </ul>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="login__social">
+                <div class="row d-flex">
+                    <div class="col-lg-6">
+                        <div class="login__social__links">
+                            <span>or</span>
+                            <ul>
+                                <li><a href="#" class="facebook"><i class="fa fa-facebook"></i> Sign in With
+                                Facebook</a></li>
+                                <li><a href="#" class="google"><i class="fa fa-google"></i> Sign in With Google</a></li>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-
             </div>
-
         </div>
     </section>
     <!-- Login Section End -->
+
 
 @include('footer')
